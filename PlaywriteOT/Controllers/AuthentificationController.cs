fb3e5c45fe4a5165c716353a0757096f88a6daac
Japhet <vannjaphet@gmail.com>
2021-06-28 07:55:00 +0200

    Add project files.

---
+++ PlaywriteOT/Controllers/AuthentificationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaywriteOT.Controllers
+{
+    public class AuthentificationController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
